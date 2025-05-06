c9555e6c296d6128bf6c72909386a7abb7bc0903
helens <helastekerova7@gmail.com>
2025-05-06 14:52:32 +0200

    map edit1

--- main2.py
+++ main2.py
@@ -107,7 +107,9 @@ fig = go.Figure(go.Scattergeo())
 fig.update_geos(projection_type="natural earth")
 fig.update_layout(height=300, margin={"r":0,"t":0,"l":0,"b":0})
 fig = px.line_geo(lat=[0,15,20,35], lon=[5,10,25,30])
-fig.update_geos(fitbounds="locations")
+fig.update_geos(visible=False, resolution=50, scope="europe",
+    showcountries=True, countrycolor="Black",
+    showsubunits=True, subunitcolor="Blue")
 fig.show()
 
 
